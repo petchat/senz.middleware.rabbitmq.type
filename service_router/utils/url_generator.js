7b031d4294ee9bf03eac4eaa6331e25dbb1fafb5
zhanghengyang <danielzhang-1@163.com>
2015-06-24 20:22:14 +0800

    fix the url bug

--- service_router/utils/url_generator.js
+++ service_router/utils/url_generator.js
@@ -7,23 +7,23 @@ var sound_url = null;
 exports.check_urls = function(){
 
     if(process.env.APP_ENV === "prod"){
-        sound_url = "http://api.trysenz.com/" +
-        "/pois/location_probability/" +
-        "/sound_pred_mfcc_gmm_url/"
-
-        motion_url = "http://api.trysenz.com/" +
-        "/utils/motion_detector/" +
-        "/motion_pred_ss_gmm_data/"
-
-        location_url = "http://api.trysenz.com/" +
+        sound_url = "http://api.trysenz.com" +
         "/utils/sound_detector/" +
-        "/senz/locationprob/"
+        "sound_pred_mfcc_gmm_url/"
+
+        motion_url = "http://api.trysenz.com" +
+        "/utils/motion_detector/" +
+        "motion_pred_ss_gmm_data/"
+
+        location_url = "http://api.trysenz.com" +
+        "/pois/location_probability/" +
+        "senz/locationprob/"
 
     }
     else{
-        sound_url = "http://api.trysenz.com/" + "/test/pois/location_probability/" + "/sound_pred_mfcc_gmm_url/"
-        motion_url = "http://api.trysenz.com/" + "/test/utils/motion_detector/" + "/motion_pred_ss_gmm_data/"
-        location_url = "http://api.trysenz.com/" + "/test/utils/sound_detector/" + "/senz/locationprob/"
+        sound_url = "http://api.trysenz.com" + "/test/utils/sound_detector/" + "sound_pred_mfcc_gmm_url/"
+        motion_url = "http://api.trysenz.com" + "/test/utils/motion_detector/" + "motion_pred_ss_gmm_data/"
+        location_url = "http://api.trysenz.com" + "/test/pois/location_probability/" + "senz/locationprob/"
 
 
     }
