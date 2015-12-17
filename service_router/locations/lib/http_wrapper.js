85783893719de344d33d3ee3364b18d40fd2c809
mageia <yzg96@gmail.com>
2015-12-17 16:28:52 +0800

    new parsehub

--- service_router/locations/lib/http_wrapper.js
+++ service_router/locations/lib/http_wrapper.js
@@ -11,7 +11,6 @@ var AV = require("avoscloud-sdk").AV;
 var _ = require("underscore");
 
 var lean_post = function (APP_ID, APP_KEY, params) {
-    params['source'] = 'test';
     var uuid = params.userRawdataId;
     logger.info(uuid, "Leancloud post started");
     var promise = new AV.Promise();
